340d5457cb088dbb1775d3bb4801d6eb83cb930d
shery15 <382254994@qq.com>
2017-09-17 23:37:40 +0800

    init

---
+++ src/components/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Pomodoro Clock, a extension for Ink-Editor.',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+
+</style>
