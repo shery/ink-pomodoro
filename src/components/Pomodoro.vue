e829cd9216e157fc420b90902f1ccd47dbbd68cb
shery15 <382254994@qq.com>
2017-09-20 23:17:39 +0800

    add value controll

--- src/components/Pomodoro.vue
+++ src/components/Pomodoro.vue
@@ -20,13 +20,21 @@ export default {
       msg: 'Welcome to Pomodoro Clock, a extension for Ink-Editor.',
       breakLen: 5,
       session: 25,
+      isBreak: false,
+      isPause: true,
+      rest: 25,
+      restSeconds: 25 * 60,
     };
   },
   methods: {
     handleBreak(value) {
+      if (!this.isPause) return;
+      if (this.breakLen === 1 && value === -1) return;
       this.breakLen += value;
     },
     handleSession(value) {
+      if (!this.isPause) return;
+      if (this.session === 1 && value === -1) return;
       this.session += value;
     },
   },
