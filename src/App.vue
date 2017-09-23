4f3ffdd1b307158c80122f557047ef9846ad3091
shery15 <382254994@qq.com>
2017-09-24 01:15:55 +0800

    feat: change root element style

--- src/App.vue
+++ src/App.vue
@@ -68,6 +68,8 @@ td, th {
 
 
 #app {
+  width: 100%;
+  height: 100%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
